59c93c40df085cf688d4c2f5ba96013ea0d06f36
Valeria Copleman <valeriacopleman@gmail.com>
2021-03-19 21:22:58 -0700

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,15 @@
 //your code here
+import React, { Component } from 'react'
+//import BlogPost from './BlogPost';
+
+class Comment extends Component {
+    render() {
+        return (
+            <div className="comment">
+                {this.props.commentText}
+            </div>
+        );
+    }
+}
+
+export default Comment;
